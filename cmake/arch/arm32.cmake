1acdc46296c9fdf3678b04504c781c1d17a47a1d
Shado <shubhambarman1221@gmail.com>
2026-08-30 14:47:17 +0530

    fix(arm32): use -marm for loader compilation so r7 is not reserved as frame pointer

--- cmake/arch/arm32.cmake
+++ cmake/arch/arm32.cmake
@@ -3,7 +3,7 @@
 message(STATUS "Configuring for ARM32 (ARMv7l)")
 
 set(LOADER_ADDR "0x20000000")
-set(LOADER_ARCH_FLAGS "-mthumb")
+set(LOADER_ARCH_FLAGS "-marm")
 set(ARCH_HAS_32BIT OFF)
 
 add_compile_definitions(
